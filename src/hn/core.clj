f98c2845e845eca0b907846de549379886188c14
Max Gonzih <gonzih@gmail.com>
2014-05-25 14:24:54 +0200

    Split new and old items in menu

--- src/hn/core.clj
+++ src/hn/core.clj
@@ -3,13 +3,24 @@
             [clojure.java.browse :refer [browse-url]]
             [clojure.java.io :refer [resource]])
   (:import [java.awt SystemTray TrayIcon PopupMenu MenuItem Toolkit]
-           [java.awt.event ActionListener])
+           [java.awt.event ActionListener]
+           [java.io IOException])
   (:gen-class))
 
-(defn menu-item [label callback]
+(def history (atom #{}))
+
+(defn is-new? [{id :id}]
+  (not (contains? @history id)))
+
+(defn mark-as-read [id]
+  (swap! history conj id))
+
+(defn menu-item [label callback & {id :id}]
   (let [menu (MenuItem. label)
         listener (proxy [ActionListener] []
-                   (actionPerformed [event] (callback)))]
+                   (actionPerformed [event]
+                     (callback)
+                     (if id (mark-as-read id))))]
     (.addActionListener menu listener)
     menu))
 
@@ -21,31 +32,44 @@
       (json/parse-string true)
       :items))
 
-(defn add-hn-to-menu! [menu]
-  (letfn [(mapfn [{:keys [title url commentCount points]}]
-            (let [full-title (format "%s - %s(%s)" title points commentCount)
-                  menu-item (menu-item full-title #(browse-url url))]
-              (println full-title)
-              (.add menu menu-item)))]
-  (doall (map mapfn (hn-items)))))
-
 (defn exit []
   (shutdown-agents)
   (System/exit 0))
+
+(defn add-spacer! [menu]
+  (.add menu (MenuItem. "-")))
+
+(defn add-exit! [menu]
+  (.add menu (menu-item "Exit" exit)))
+
+(defn add-hn-to-menu! [menu]
+  (let [{new-items true old-items false} (group-by is-new? (hn-items))]
+    (letfn [(mapfn [{:keys [id title url commentCount points]}]
+              (let [full-title (format "%s - %s (%s)" title points commentCount)
+                    menu-item (menu-item full-title #(browse-url url) :id id)]
+                (println full-title)
+                (.add menu menu-item)))]
+      (doall (map mapfn new-items))
+      (if (seq old-items)
+        (add-spacer! menu))
+      (doall (map mapfn old-items)))))
 
 (defn -main [& args]
   (let [tray (SystemTray/getSystemTray)
         image (.getImage (Toolkit/getDefaultToolkit)
                          (resource "icon.png"))
-        icon (TrayIcon. image)
-        exit (menu-item "Exit" exit)]
+        icon (TrayIcon. image) ]
     (.setImageAutoSize icon true)
     (.add tray icon)
     (loop []
-      (let [popup (PopupMenu.)]
-        (println "Updating items")
-        (add-hn-to-menu! popup)
-        (.add popup exit)
-        (.setPopupMenu icon popup)
-        (Thread/sleep (* 5 60 1000))
-        (recur)))))
+      (try
+        (let [popup (PopupMenu.)]
+          (println "Updating items")
+          (add-hn-to-menu! popup)
+          (add-spacer! popup)
+          (add-exit! popup)
+          (.setPopupMenu icon popup)
+          (Thread/sleep (* 5 60 1000)))
+        (catch IOException e
+          (println (str "Exception during update " e))))
+      (recur))))
