d469510344f890a39bd7294b48316cb489657b68
Max Gonzih <gonzih@gmail.com>
2014-05-25 13:20:21 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hn "0.1.0-SNAPSHOT"
+(defproject hn "0.1.1-SNAPSHOT"
   :description "HN Tray"
   :url "https://github.com/Gonzih/hn-tray.clj"
   :license {:name "Eclipse Public License"
